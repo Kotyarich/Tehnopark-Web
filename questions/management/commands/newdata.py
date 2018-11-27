7e80d01181e60bb2aca64bb72ee81928ad3d5d90
knd17u684 <kotovnd@mail.ru>
2018-11-27 12:16:35 +0300

    Generation of likes added

--- questions/management/commands/newdata.py
+++ questions/management/commands/newdata.py
@@ -3,7 +3,7 @@ import random
 from django.core.management.base import BaseCommand, CommandError
 from django.db import IntegrityError
 from questions.models import Question as Poll
-from questions.models import Question, User, Tag, Answer, QuestionLike, Profile
+from questions.models import Question, User, Tag, Answer, Like, Profile
 from faker import Faker
 from random import randint
 
@@ -20,8 +20,12 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.create_tags()
         self.create_profiles()
+        users = User.objects.all()
         self.create_questions()
         self.create_answers()
+        questions = Question.objects.all()
+        answers = Answer.objects.all()
+        self.create_likes(users, questions, answers)
 
     def create_tags(self):
         if Tag.objects.count() > 0:
@@ -35,21 +39,27 @@ class Command(BaseCommand):
         if User.objects.count() > 0:
              return
 
-        user = User.objects.create_user('user1', 'user@mail.ru', '1234')
+        user = User.objects.create_superuser('user1', 'user@mail.ru', '1234')
         user.save()
         p = Profile.objects.create(user=user)
         p.save()
+        for i in range(15):
+            user = User.objects.create_user(
+                username=str(i)+str(i),
+                password='pass{}'.format(i),
+                email=str(i) + faker.email())
+            user.save()
 
     def create_questions(self):
         if Question.objects.count() > 0:
             return
 
         tags = Tag.objects.all()
-        for i in range(1000):
+        for i in range(25):
             question = Question.objects.create(title=faker.sentence(),
                                                text=faker.text(),
                                                author=User.objects.first(),
-                                               rating=random.randint(-2, 20))
+                                               rating=0)
             for j in range(3):
                 tag = random.choice(tags)
                 question.tags.add(tag)
@@ -59,6 +69,24 @@ class Command(BaseCommand):
         if Answer.objects.count() > 0:
             return
         for question in Question.objects.all():
-            for i in range(random.randint(1, 20)):
+            for i in range(random.randint(1, 15)):
                 a = Answer.objects.create(text=faker.text(), question=question, author=User.objects.first())
                 a.save()
+
+    def create_likes(self, users, questions, answers):
+        for user in users:
+            qs = random.sample(list(questions), 10)
+            for q in qs:
+                value = random.choice([1, -1])
+                like = Like(value=value, user=user, content_object=q)
+                like.save()
+                like.content_object.rating += like.value
+                like.content_object.save(update_fields=['rating'])
+
+            ans = random.sample(list(answers), 40)
+            for a in ans:
+                value = random.choice([1, -1])
+                like = Like(value=value, user=user, content_object=a)
+                like.save()
+                like.content_object.rating += like.value
+                like.content_object.save(update_fields=['rating'])
