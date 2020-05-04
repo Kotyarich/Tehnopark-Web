a166ef66cf65bfbd1b5ce60adda02725a2c54d06
Kotyarich <ndkotov@gmail.com>
2020-05-04 09:04:42 +0300

    change fill script

--- questions/management/commands/newdata.py
+++ questions/management/commands/newdata.py
@@ -1,11 +1,9 @@
 import random
 
-from django.core.management.base import BaseCommand, CommandError
-from django.db import IntegrityError
-from questions.models import Question as Poll
-from questions.models import Question, User, Tag, Answer, Like, Profile
+from django.core.management.base import BaseCommand
 from faker import Faker
-from random import randint
+
+from questions.models import Question, User, Tag, Answer, Like, Profile
 
 faker = Faker()
 
@@ -28,31 +26,33 @@ class Command(BaseCommand):
         self.create_likes(users, questions, answers)
 
     def create_tags(self):
-        if Tag.objects.count() > 0:
-            return
+        # if Tag.objects.count() > 0:
+        #     return
 
         for i in range(40):
             tag = Tag.objects.create(text=faker.word() + str(i))
             tag.save()
 
     def create_profiles(self):
-        if User.objects.count() > 0:
-             return
+        # if User.objects.count() > 0:
+        #     return
 
         user = User.objects.create_superuser('user1', 'user@mail.ru', '1234')
         user.save()
-        p = Profile.objects.create(user=user)
+        p = Profile.objects.create(user=user, nickname='nickname')
         p.save()
         for i in range(40):
             user = User.objects.create_user(
-                username=str(i)+str(i),
+                username=str(i) + str(i),
                 password='pass{}'.format(i),
                 email=str(i) + faker.email())
             user.save()
+            p = Profile.objects.create(user=user, nickname='nickname' + str(i))
+            p.save()
 
     def create_questions(self):
-        if Question.objects.count() > 0:
-            return
+        # if Question.objects.count() > 0:
+        #     return
 
         tags = Tag.objects.all()
         for i in range(101):
@@ -68,11 +68,12 @@ class Command(BaseCommand):
             question.save()
 
     def create_answers(self):
-        if Answer.objects.count() > 0:
-            return
+        # if Answer.objects.count() > 0:
+        #     return
         for question in Question.objects.all():
             for i in range(random.randint(1, 25)):
-                a = Answer.objects.create(text=faker.text(), question=question, author=User.objects.first())
+                a = Answer.objects.create(text=faker.text(), question=question,
+                                          author=User.objects.first())
                 a.save()
 
     def create_likes(self, users, questions, answers):
